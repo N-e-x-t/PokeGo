a2a99a3d4145cc34a6f8768a58b8e0512c30037c
Jigar Parekh <jigarparekh002@gmail.com>
2017-04-06 15:30:20 +0530

    Initialized Pokedox

---
+++ PokéGo/CDHelper.swift
@@ -0,0 +1,20 @@
+//
+//  CDHelper.swift
+//  PokéGo
+//
+//  Created by Jigar Parekh on 06/04/17.
+//  Copyright © 2017 Next. All rights reserved.
+//
+
+import CoreData
+import UIKit
+
+func makeAllPokemon() {
+    
+    }
+
+func makePokemon(name: String, withThe imageName: String) {
+    
+    
+    
+}
---
+++ PokéGo/PokedoxViewController.swift
@@ -0,0 +1,59 @@
+//
+//  PokedoxViewController.swift
+//  PokéGo
+//
+//  Created by Jigar Parekh on 06/04/17.
+//  Copyright © 2017 Next. All rights reserved.
+//
+
+import UIKit
+
+class PokedoxViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    @IBAction func backToMapButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // TableView Function
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Int()
+    }
+    
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
