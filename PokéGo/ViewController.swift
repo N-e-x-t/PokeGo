b5d3fd9b979d134831f209d202356c607a3081e0
Jigar Parekh <jigarparekh002@gmail.com>
2017-04-06 12:05:16 +0530

    Added MapKit with user location

--- PokéGo/ViewController.swift
+++ PokéGo/ViewController.swift
@@ -7,12 +7,59 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBAction func userLocationRepositioningButtonPress(_ sender: Any) {
+        // Sets the region of view to display
+        let region = MKCoordinateRegionMakeWithDistance(self.manager.location!.coordinate, 400, 400)
+        self.mapView.setRegion(region, animated: true)
+    }
+    
+    var manager = CLLocationManager()
+    
+    var update = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.manager.delegate = self
+        
+        
+        //Authorization status and location setting
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            
+            //Shows the blue spot for user location
+            self.mapView.showsUserLocation = true
+            
+            //Updates the user location
+            self.manager.startUpdatingLocation()
+            
+        }
+        else {
+            self.manager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if update < 4{
+        
+        // Sets the region of view to display
+        let region = MKCoordinateRegionMakeWithDistance(self.manager.location!.coordinate, 400, 400)
+        self.mapView.setRegion(region, animated: true)
+        self.manager.startUpdatingLocation()
+            //update += 4
+        }
+        else {
+            self.manager.stopUpdatingLocation()
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
