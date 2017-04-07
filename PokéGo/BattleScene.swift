729d1c99edb8fdb8c6a0adf023fdeca7a73a1fee
Jigar Parekh <jigarparekh002@gmail.com>
2017-04-07 12:45:46 +0530

    Added Battle scene background

--- PokéGo/BattleScene.swift
+++ PokéGo/BattleScene.swift
@@ -16,9 +16,20 @@ class BattleScene : SKScene, SKPhysicsContactDelegate {
     //To move/load the game scene in the view
     override func didMove(to view: SKView) {
         
-        print("Welcome to the PokéGo Battle")
+        //print("Welcome to the PokéGo Battle")
+        //print(pokemon.name)
         
-        print(pokemon.name)
+        
+        //Background Code
+        
+        let battleBg = SKSpriteNode(imageNamed: "bggg")
+        
+        battleBg.size = self.size
+        battleBg.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        battleBg.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        battleBg.zPosition = -1
+        self.addChild(battleBg)
+        
     }
     
 }
